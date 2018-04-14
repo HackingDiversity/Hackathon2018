6e682ca2fae9655d5853b0f6327e59132f851777
Sadiyah Faruk <sf2ne@virginia.edu>
2018-04-14 12:27:36 -0400

    Updated js object to reflect data in calendar

--- webapp/Chalk_and_Slate/student_data.js
+++ webapp/Chalk_and_Slate/student_data.js
@@ -1,3 +1,5 @@
+// This is just for reference of what information is reflected
+// in the calendar, so that everything matches up
 var student = {
   'first_name': 'Sarah',
   'last_name': 'P.',
@@ -11,9 +13,9 @@ var student = {
     'address' : '71 Pilgrim Avenue Chevy Chase, MD 20815'
     'email_address' : 'sp6fks2020@gmail.com'
     'rotations' : {
-          '1' : 'Hermatology',
-          '2' : 'Mind, Brain, & Behavior'
-          '3' : 'Cells to Society'
-          '4' : 'Cardiovascular System'
+          '1' : 'Pediatrics',
+          '2' : 'Emergency'
+          '3' : 'ICU'
+          '4' : 'Neonatal'
       },
 }

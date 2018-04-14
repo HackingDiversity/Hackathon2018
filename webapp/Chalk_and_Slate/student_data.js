a472001001d028062fee46b1e677926e129951c6
Sadiyah Faruk <sf2ne@virginia.edu>
2018-04-14 12:19:44 -0400

    Added google calendar, and beginning of student_data.js

---
+++ webapp/Chalk_and_Slate/student_data.js
@@ -0,0 +1,19 @@
+var student = {
+  'first_name': 'Sarah',
+  'last_name': 'P.',
+  'enrolled_classes' : {
+        '1' : 'Hermatology',
+        '2' : 'Mind, Brain, & Behavior'
+        '3' : 'Cells to Society'
+        '4' : 'Cardiovascular System'
+    },
+    'username' : 'sp6fks2020'
+    'address' : '71 Pilgrim Avenue Chevy Chase, MD 20815'
+    'email_address' : 'sp6fks2020@gmail.com'
+    'rotations' : {
+          '1' : 'Hermatology',
+          '2' : 'Mind, Brain, & Behavior'
+          '3' : 'Cells to Society'
+          '4' : 'Cardiovascular System'
+      },
+}
